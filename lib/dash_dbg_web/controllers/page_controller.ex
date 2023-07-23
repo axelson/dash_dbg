f2f89044e91b657574cd704f6ff781dc091f29de
Jason Axelson <jason.axelson@gmail.com>
2023-07-23 07:27:43 -1000

    Start implementation

--- lib/dash_dbg_web/controllers/page_controller.ex
+++ lib/dash_dbg_web/controllers/page_controller.ex
@@ -1,9 +1,25 @@
 defmodule DashDbgWeb.PageController do
   use DashDbgWeb, :controller
+  require Logger
 
   def home(conn, _params) do
-    # The home page is often custom made,
-    # so skip the default app layout.
-    render(conn, :home, layout: false)
+    # url = "http://localhost:8000/capture.ppm"
+    url = "http://inky.local/capture.ppm"
+
+    capture_ppm =
+      case Req.get(url) do
+        {:ok, resp} ->
+          resp.body
+
+        error ->
+          Logger.warn("Error fetching capture: #{inspect(error)}")
+          nil
+      end
+
+    conn
+    |> Plug.Conn.put_resp_content_type("image/png")
+    |> Plug.Conn.send_resp(200, capture_ppm)
+
+    # render(conn, :home, layout: false)
   end
 end
